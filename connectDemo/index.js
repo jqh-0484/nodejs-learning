c658c81a2cd70f58b9a367bdb33d8512571ccebc
jiaqiuhua <flower346@qq.com>
2020-09-15 11:52:52 +0800

    测试git使用

--- connectDemo/index.js
+++ connectDemo/index.js
@@ -1,25 +1,26 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 //创建连接对象
 const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    port: '3306',
-    database: 'myblog'
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  port: "3306",
+  database: "myblog",
 });
 
 //开始连接
-con.connect()
+con.connect();
 
 //执行sql语句
 const sql = `insert into blogs (title,content,createtime,author) values('标题c','这是标题c的内容',1587867748561,'张三')`;
 con.query(sql, (err, result) => {
-    if(err){
-        console.log(err);
-        return
-    }
-    console.log(result);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(result);
+  console.log("这里是测试git使用的");
 });
 
 //关闭连接
