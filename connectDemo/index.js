d9c8f865ead7e48e0419bafb18b824231cc28ef6
jiaqiuhua <flower346@qq.com>
2020-09-15 14:22:18 +0800

    我在拉取master分支上最新代码的情况下，在dev分支上修改了代码，并提交到dev分支上，合并代码到master分支的时候会冲突

--- connectDemo/index.js
+++ connectDemo/index.js
@@ -25,6 +25,9 @@ con.query(sql, (err, result) => {
   console.log(
     "我是在dev分支上进行开发呢，我现在要提交dev的代码到dev分支上，然后在把它合到master分支上，最后在删除这个dev分支"
   );
+  console.log(
+    "刚刚master分支上的代码做了修改，但是我在切换到dev分支上进行开发之后，并没有拉取最新的master分支上的代码，这时，当我在dev分支上开发完并将代码提交到dev分支上的时候，我再切换到master分支把dev分支上的代码合并到master分支上的时候，代码合并就会冲突，此时应该解决冲突"
+  );
 });
 
 //关闭连接
